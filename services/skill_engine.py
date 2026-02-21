00c05dde70bf2821a4844559bbfdb40ba800d08c
NamrataP28 <namrataprasad28@gmail.com>
2026-02-21 23:37:43 +0530

    Update skill_engine.py

--- services/skill_engine.py
+++ services/skill_engine.py
@@ -1,27 +1,23 @@
-from sentence_transformers import SentenceTransformer
-from sklearn.metrics.pairwise import cosine_similarity
 import re
-
-model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def extract_keywords(text):
     words = re.findall(r'\b[a-zA-Z]{4,}\b', text.lower())
     return list(set(words))
 
-def compute_skill_gap(resume_text, job_descriptions):
+def compute_skill_gap(resume_text, job_list):
 
-    resume_keywords = extract_keywords(resume_text)
+    resume_kw = extract_keywords(resume_text)
 
-    job_keywords = []
-    for job in job_descriptions:
+    job_kw = []
+    for job in job_list:
         desc = job.get("job_description", "")
-        job_keywords += extract_keywords(desc)
+        job_kw += extract_keywords(desc)
 
-    job_keywords = list(set(job_keywords))
+    job_kw = list(set(job_kw))
 
-    matched = list(set(resume_keywords) & set(job_keywords))
-    missing = list(set(job_keywords) - set(resume_keywords))
+    matched = list(set(resume_kw) & set(job_kw))
+    missing = list(set(job_kw) - set(resume_kw))
 
-    match_score = (len(matched) / len(job_keywords) * 100) if job_keywords else 0
+    score = len(matched) / len(job_kw) * 100 if job_kw else 0
 
-    return round(match_score,2), missing[:15]
+    return round(score, 2), missing[:20]
