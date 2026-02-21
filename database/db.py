e12a1a351ebc368f4ac0d647ab8fc81ad44bd49e
NamrataP28 <namrataprasad28@gmail.com>
2026-02-21 21:41:52 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,6 +1,46 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+import sqlite3
 
-engine = create_engine("sqlite:///database.db")
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
+def init_db():
+    conn = sqlite3.connect("career.db")
+    c = conn.cursor()
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        email TEXT PRIMARY KEY,
+        password TEXT
+    )
+    """)
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS sessions (
+        email TEXT,
+        target_role TEXT,
+        country TEXT,
+        opportunity_score REAL,
+        percentile REAL
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+def authenticate_user(email, password):
+    conn = sqlite3.connect("career.db")
+    c = conn.cursor()
+    c.execute(
+        "SELECT * FROM users WHERE email=? AND password=?",
+        (email, password)
+    )
+    result = c.fetchone()
+    conn.close()
+    return result
+
+def register_user(email, password):
+    conn = sqlite3.connect("career.db")
+    c = conn.cursor()
+    c.execute(
+        "INSERT OR IGNORE INTO users VALUES (?,?)",
+        (email, password)
+    )
+    conn.commit()
+    conn.close()
