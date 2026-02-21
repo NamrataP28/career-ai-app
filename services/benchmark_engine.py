1e44677525cc41449b2f9b9e39618b3982c6ebdd
NamrataP28 <namrataprasad28@gmail.com>
2026-02-21 23:38:19 +0530

    Update benchmark_engine.py

--- services/benchmark_engine.py
+++ services/benchmark_engine.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 def percentile_rank(user_score):
-    market_distribution = np.random.normal(60, 15, 1000)
-    percentile = sum(market_distribution < user_score) / len(market_distribution) * 100
-    return round(percentile,2)
+    distribution = np.random.normal(60, 15, 1000)
+    percentile = sum(distribution < user_score) / len(distribution) * 100
+    return round(percentile, 2)
