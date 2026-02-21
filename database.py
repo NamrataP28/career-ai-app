e0cddcc5137f037f9cdb52cd90cc05e9a1823dbc
NamrataP28 <namrataprasad28@gmail.com>
2026-02-21 18:36:27 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("career.db")
+    c = conn.cursor()
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        email TEXT PRIMARY KEY,
+        password TEXT
+    )
+    """)
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS sessions (
+        email TEXT,
+        target_role TEXT,
+        country TEXT,
+        opportunity_score REAL,
+        percentile REAL
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+def authenticate_user(email, password):
+    conn = sqlite3.connect("career.db")
+    c = conn.cursor()
+    c.execute("SELECT * FROM users WHERE email=? AND password=?", (email, password))
+    result = c.fetchone()
+    conn.close()
+    return result
+
+def register_user(email, password):
+    conn = sqlite3.connect("career.db")
+    c = conn.cursor()
+    c.execute("INSERT OR IGNORE INTO users VALUES (?,?)", (email, password))
+    conn.commit()
+    conn.close()
